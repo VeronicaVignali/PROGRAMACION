9a059c49de041014b4b2c0f5df1019c0631cfe00
VeronicaVignali <vero.vigna98@gmail.com>
2025-10-04 19:14:30 +0200

    update

---
+++ PrimerProyecto/src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+
+    //esto es un comentario
+    /* este es un ccomentario
+    que se puede dividir
+    en varias lienas de codigo
+     */
+    //TODO me falta esto
+
+    public static void main(String[] args) {
+        System.out.println("Esta es mi primer programa en java");
+        System.out.println("Esta es la segunda linea del programa");
+
+    }
+    //TODO esto tambien me queda pendiente 
+}
